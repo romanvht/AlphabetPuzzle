676422fb86aafbee35d3243cccd082015c638b69
romanvht <roman.vkostin@gmail.com>
2018-10-11 16:15:55 +1000

    Optimize SQL

--- post.php
+++ post.php
@@ -7,6 +7,9 @@ $name = in_t($_POST['name']);
 $size = int($_POST['size']);
 $score = int($_POST['score']);
 $win = int($_POST['win']);
-if(!empty($name) && !empty($size) && !empty($score))
-$vht->query("INSERT INTO `2048` (`name`, `size`, `score`, `win`, `time`) VALUES ('$name', '$size', '$score', '$win', '".time()."')");
+if(!empty($name) && !empty($size) && !empty($score)){
+	$sql = $vht->prepare("INSERT INTO `2048` (name, size, score, win, time) VALUES (?,?,?,?,?)");
+	$sql->bind_param("siiii", $name, $size, $score, $win, time());
+	$sql->execute();
+}
 exit();
